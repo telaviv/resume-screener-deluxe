fd4993b24be7d6bb9791dfc67f85ee3194f46e53
Shawn Krisman <krisman.shawn@gmail.com>
2014-12-06 20:24:22 -0800

    changed the background.

--- js/game.js
+++ js/game.js
@@ -9,17 +9,17 @@ Crafty.scene('StartScene', function() {
 
 Crafty.c('Screen', {
     init: function() {
-        this.requires('2D, DOM, Color');
+        this.requires('2D, DOM, Image, Stage');
         this.attr({x: 0, y: 0, w: 640, h: 400});
-        this.color('red');
         this.attach(Crafty.e('Resume'));
+        this.image('img/background.png');
     }
 });
 
 Crafty.c('Window', {
     init: function() {
         this.requires('2D, DOM, Color');
-        this.attr({x: 15, y: 15, w: 400, h: 300});
+        this.attr({x: 40, y: 40, w: 400, h: 300});
         this.color('white');
     }
 });
@@ -32,8 +32,8 @@ Crafty.c('Resume', {
 
     _createName: function() {
         return Crafty.e("2D, DOM, Text").attr({
-            x: this.x + 10, y: this.y + 10, w: this.w
-        }).text(NAMES[0]).textColor('black');
+            x: this.x + 40, y: this.y + 40, w: this.w
+        }).text(NAMES[0]).textColor('black').textFont({size: '15px'});
     }
 });
 
