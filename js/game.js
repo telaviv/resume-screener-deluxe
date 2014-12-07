fde039921d93f188f7928af4eb25bc64b8aa0140
Shawn Krisman <krisman.shawn@gmail.com>
2014-12-06 20:07:06 -0800

    first name added.

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ Crafty.c('Screen', {
         this.requires('2D, DOM, Color');
         this.attr({x: 0, y: 0, w: 640, h: 400});
         this.color('red');
-        this.attach(Crafty.e('Window'));
+        this.attach(Crafty.e('Resume'));
     }
 });
 
@@ -23,6 +23,20 @@ Crafty.c('Window', {
         this.color('white');
     }
 });
+
+Crafty.c('Resume', {
+    init: function() {
+        this.requires('Window');
+        this.attach(this._createName());
+    },
+
+    _createName: function() {
+        return Crafty.e("2D, DOM, Text").attr({
+            x: this.x + 10, y: this.y + 10, w: this.w
+        }).text(NAMES[0]).textColor('black');
+    }
+});
+
 
 var NAMES = ['John Smith'];
 var PROGRAMMING_LANGUAGES = ['python'];
