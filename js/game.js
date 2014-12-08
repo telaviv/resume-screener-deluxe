eaf04009207ab689817901f441c4dc350c1cfaee
Shawn Krisman <krisman.shawn@gmail.com>
2014-12-07 19:15:35 -0800

    more added to the resume.

--- js/game.js
+++ js/game.js
@@ -27,22 +27,51 @@ Crafty.c('Window', {
 Crafty.c('Resume', {
     init: function() {
         this.requires('Window');
-        this.attach(this._createName());
-        this.attach(this._createProgrammingLanguages());
+        this._attach(this._createName(20));
+        this._attach(this._createLineRow(40));
+        this._attach(this._createEducation(60));
+        this._attach(this._createLineRow(80));
+        this._attach(this._createExperience(100));
+        this._attach(this._createProgrammingLanguages(180));
     },
 
-    _createName: function() {
-        return this._createText(NAMES[0], 40);
+    _createName: function(yOffset) {
+        return this._createText(NAMES[0], yOffset, 18);
     },
 
-    _createProgrammingLanguages: function() {
-        return this._createText('Programming Languages: ' + PROGRAMMING_LANGUAGES[0], 90);
+    _createEducation: function(yOffset) {
+        return this._createText('Education: ' + DEGREE[0] + ', ' + SCHOOLS[0], yOffset);
     },
 
-    _createText: function(text, yOffset) {
+    _createProgrammingLanguages: function(yOffset) {
+        return this._createText('Programming Languages: ' + PROGRAMMING_LANGUAGES[0], yOffset);
+    },
+
+    _createExperience: function(yOffset) {
+        var text = [];
+        text.push(this._createText(POSITION[0], yOffset, 18));
+        text.push(this._createText(COMPANY[0], yOffset + 40));
+        return text;
+    },
+
+    _createLineRow: function(yOffset) {
+        return this._createText('_________________________________________', yOffset);
+    },
+
+    _createText: function(text, yOffset, size) {
+        size = size || 15;
         return Crafty.e("2D, DOM, Text").attr({
             x: this.x + 40, y: this.y + yOffset, w: this.w
-        }).text(text).textColor('black').textFont({size: '15px'});
+        }).text(text).textColor('black').textFont({size: size + 'px'});
+    },
+
+    _attach: function(text) {
+        if (text instanceof Array) {
+            text.forEach(function(textComponent) {
+                this.attach(textComponent);
+            }.bind(this));
+        }
+        this.attach(text);
     }
 });
 
@@ -50,3 +79,6 @@ Crafty.c('Resume', {
 var NAMES = ['John Smith'];
 var PROGRAMMING_LANGUAGES = ['python'];
 var SCHOOLS = ['UC Berkeley'];
+var DEGREE = ['B.S. in Computer Science'];
+var POSITION = ['Software Engineering Intern'];
+var COMPANY = ['bitely INC.'];
